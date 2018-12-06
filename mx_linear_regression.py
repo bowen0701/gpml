037d8509d342c6fedcba6c247def82858a46b624
Bowen Li <bowen0701@gmail.com>
2018-12-06 22:42:50 +0100

    Add mx_linear_regression.py

---
+++ mx_linear_regression.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from mxnet import nd, autograd
+import random
+
+
+class LinearRegression(object):
+    """A MXNet implementation of Linear Regression."""
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
