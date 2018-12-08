bf224559dd42c814e99ec9c93a8c04e97b8e1e26
Bowen Li <bowen0701@gmail.com>
2018-12-08 16:24:49 +0100

    Complete mx_linear_regression_gluon.py

--- mx_linear_regression_gluon.py
+++ mx_linear_regression_gluon.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from mxnet import nd, autogrd, gluon
+from mxnet import nd, autograd, init, gluon
 from mxnet.gluon import data as gdata
 from mxnet.gluon import nn
 from mxnet.gluon import loss as gloss
@@ -10,25 +10,50 @@ from mxnet.gluon import loss as gloss
 
 class LinearRegression(object):
     def __init__(self, batch_size=10, lr=0.01, num_epochs=5):
-        pass
+        self.batch_size = batch_size
+        self.lr = lr
+        self.num_epochs = num_epochs
 
     def data_iter(self):
-        pass
+        dataset = gdata.ArrayDataset(self.features, self.labels)
+        return gdata.DataLoader(dataset, self.batch_size, shuffle=True)
 
     def linreg(self):
-        pass
+        net = nn.Sequential()
+        net.add(nn.Dense(1))
+        return net
 
-    def weights_init(self):
-        pass
+    def weights_init(self, net):
+        net.initialize(init.Normal(sigma=0.01))
 
     def squared_loss(self):
-        pass
+        return gloss.L2Loss()
 
-    def trainer_sgd(self):
-        pass
+    def sgd_trainer(self, net):
+        return gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': self.lr})
 
     def fit(self, features, labels):
-        pass
+        self.features = features
+        self.labels = labels
+
+        net = self.linreg()
+        self.weights_init(net)
+        loss = self.squared_loss()
+        trainer = self.sgd_trainer(net)
+
+        for epoch in list(range(self.num_epochs)):
+            for X, y in self.data_iter():
+                with autograd.record():
+                    l = loss(net(X), y)
+                l.backward()
+                trainer.step(self.batch_size)
+
+            train_loss = loss(net(self.features), self.labels)
+            print('epoch {0}: loss {1}'
+                  .format(epoch + 1, train_loss.mean().asnumpy()))
+
+        self.net = net
+        return self
 
 
 def main():
@@ -42,7 +67,11 @@ def main():
     labels = nd.dot(features, true_w) + true_b
     labels += nd.random.normal(scale=0.01, shape=labels.shape)
 
-    pass
+    linreg = LinearRegression(batch_size=10, lr=0.02, num_epochs=5)
+    linreg.fit(features, labels)
+
+    print('w, true_w: {0}, {1}'.format(linreg.net[0].weight.data(), true_w))
+    print('b, true_b: {0}, {1}'.format(linreg.net[0].bias.data(), true_b))
 
 
 if __name__ == '__main__':
