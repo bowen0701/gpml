d0a7384de606e119299113114d267c57a9f1e3ac
Bowen Li <bowen0701@gmail.com>
2018-12-17 22:47:55 +0100

    Complete inner_product() & cosine_similarity()

--- np_metrics.py
+++ np_metrics.py
@@ -6,8 +6,29 @@ import math
 import numpy as np
 
 
+def inner_product(x, y):
+    return x.dot(y)
+
+
+def cosine_similarity(x, y):
+    numerator = inner_product(x, y)
+    denominator = math.sqrt(inner_product(x, x) * inner_product(y, y))
+    return numerator / denominator
+
+
 def main():
-	pass
+    import time
+
+    x = np.array([1, 2, 3])
+    y = np.array([2, 3, 4])
+
+    start_time = time.time()
+    print('Inner product: {}'.format(inner_product(x, y)))
+    print('Time: {}'.format(time.time() - start_time))
+
+    start_time = time.time()
+    print('Cosine similarity: {}'.format(cosine_similarity(x, y)))
+    print('Time: {}'.format(time.time() - start_time))
 
 if __name__ == '__main__':
-	main()
+    main()
