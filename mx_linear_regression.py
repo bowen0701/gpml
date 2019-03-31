06c322b6c01b97235bec7f44dadf2bd839b0a37d
bowen0701 <bowen0701@gmail.com>
2019-04-01 00:44:11 +0200

    Replace by X_train & y_train, add coef() & predict()

--- mx_linear_regression.py
+++ mx_linear_regression.py
@@ -14,12 +14,11 @@ class LinearRegression(object):
         self.num_epochs = num_epochs
 
     def _data_iter(self):
-        num_examples = self.features.shape[0]
-        idx = list(range(num_examples))
+        idx = list(range(self.num_examples))
         random.shuffle(idx)
-        for i in range(0, num_examples, self.batch_size):
+        for i in range(0, self.num_examples, self.batch_size):
             idx_batch = nd.array(idx[i:min(i + self.batch_size, self.num_examples)])
-            yield self.features.take(idx_batch), self.labels.take(idx_batch)
+            yield self.X_train.take(idx_batch), self.y_train.take(idx_batch)
     
     def _linreg(self, X, w, b):
         return nd.dot(X, w) + b
@@ -41,11 +40,11 @@ class LinearRegression(object):
             # Take parameter's gradient from auto diff output.
             param[:] = param - self.lr * param.grad / self.batch_size
     
-    def fit(self, features, labels):
-        self.features = features
-        self.labels = labels
-        self.num_examples = features.shape[0]
-        self.num_inputs = features.shape[1]
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.num_examples = X_train.shape[0]
+        self.num_inputs = X_train.shape[1]
                 
         net = self._linreg
         w, b = self._weights_init()
@@ -59,31 +58,17 @@ class LinearRegression(object):
                 l.backward()
                 self._sgd(w, b)
 
-            train_loss = loss(net(self.features, w, b), labels)
+            train_loss = loss(net(self.X_train, w, b), y_train)
             print('epoch {0}: loss {1}'
                   .format(epoch + 1, train_loss.mean().asnumpy()))
         
+        self.net = net
         self.w, self.b = w, b
         return self
 
+    # TODO: Fix bug for coef.
+    def coef(self):
+        return [self.b] + self.w
 
-def main():
-    true_w = nd.array([2, -3.4])
-    true_b = 4.2
-
-    num_examples = 1000
-    num_inputs = len(true_w)
-
-    features = nd.random.normal(scale=1, shape=(num_examples, num_inputs))
-    labels = nd.dot(features, true_w) + true_b
-    labels += nd.random.normal(scale=0.01, shape=labels.shape)
-
-    linreg = LinearRegression(batch_size=10, lr=0.02, num_epochs=5)
-    linreg.fit(features, labels)
-    
-    print('w, true_w: {0}, {1}'.format(linreg.w, true_w))
-    print('b, true_b: {0}, {1}'.format(linreg.b, true_b))
-
-
-if __name__ == '__main__':
-    main()
+    def predict(self, X_test):
+        return self.net(X_test, self.w, self.b)
