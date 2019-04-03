4d66a25f28ade691ba3a05d887d45e158b32169a
bowen0701 <bowen0701@gmail.com>
2019-04-03 08:48:42 +0200

    Revise vars to n_inputs & n_examples

--- mx_linear_regression.py
+++ mx_linear_regression.py
@@ -7,24 +7,24 @@ import random
 
 
 class LinearRegression(object):
-    """A MXNet implementation of Linear Regression."""
-    def __init__(self, batch_size=10, lr=0.01, num_epochs=5):
+    """MXNet implementation of Linear Regression."""
+    def __init__(self, batch_size=10, lr=0.01, n_epochs=5):
         self.batch_size = batch_size
         self.lr = lr
-        self.num_epochs = num_epochs
+        self.n_epochs = n_epochs
 
     def _data_iter(self):
-        idx = list(range(self.num_examples))
+        idx = list(range(self.n_examples))
         random.shuffle(idx)
-        for i in range(0, self.num_examples, self.batch_size):
-            idx_batch = nd.array(idx[i:min(i + self.batch_size, self.num_examples)])
+        for i in range(0, self.n_examples, self.batch_size):
+            idx_batch = nd.array(idx[i:min(i + self.batch_size, self.n_examples)])
             yield self.X_train.take(idx_batch), self.y_train.take(idx_batch)
     
     def _linreg(self, X, w, b):
         return nd.dot(X, w) + b
 
     def _weights_init(self):
-        w = nd.random.normal(scale=0.01, shape=(self.num_inputs, 1))
+        w = nd.random.normal(scale=0.01, shape=(self.n_inputs, 1))
         b = nd.zeros(shape=(1,))
         params = [w, b]
         for param in params:
@@ -43,14 +43,14 @@ class LinearRegression(object):
     def fit(self, X_train, y_train):
         self.X_train = X_train
         self.y_train = y_train
-        self.num_examples = X_train.shape[0]
-        self.num_inputs = X_train.shape[1]
+        self.n_examples = X_train.shape[0]
+        self.n_inputs = X_train.shape[1]
                 
         net = self._linreg
         w, b = self._weights_init()
         loss = self._squared_loss
 
-        for epoch in range(self.num_epochs):
+        for epoch in range(self.n_epochs):
             for X, y in self._data_iter():
                 # Record auto diff & perform backward differention.
                 with autograd.record():
