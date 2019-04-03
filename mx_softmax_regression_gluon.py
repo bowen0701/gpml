765bbb4626105600a51fbe624bbefde01285a592
bowen0701 <bowen0701@gmail.com>
2019-04-03 08:50:05 +0200

    Add __init__

--- mx_softmax_regression_gluon.py
+++ mx_softmax_regression_gluon.py
@@ -2,10 +2,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from mxnet import nd, autograd
+import random
 
-def main():
-    pass
 
-
-if __name__ == '__main__':
-    main()
+class SoftmaxRegression(object):
+	"""MXNet implementation of Softmax Regression."""
+	def __init__(self, batch_size=10, lr=0.01, n_epoches):
+		self.batch_size = batch_size
+		self.lr = lr
+		self.n_epoches = n_epoches
