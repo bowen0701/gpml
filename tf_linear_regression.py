280ba9688ab166a648ab2b2beb79dbb54c6e5977
bowen0701 <bowen0701@gmail.com>
2019-04-18 22:46:48 +0200

    Remove main()

--- tf_linear_regression.py
+++ tf_linear_regression.py
@@ -8,19 +8,3 @@ import tensorflow as tf
 class LinearRegression(object):
     """A TensorFlow implementation of Linear Regression."""
     pass
-
-
-def main():
-    true_w = nd.array([2, -3.4])
-    true_b = 4.2
-
-    num_examples = 1000
-    num_inputs = len(true_w)
-
-    features = nd.random.normal(scale=1, shape=(num_examples, num_inputs))
-    labels = nd.dot(features, true_w) + true_b
-    labels += nd.random.normal(scale=0.01, shape=labels.shape)
-
-
-if __name__ == '__main__':
-    main()
