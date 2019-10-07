f75010c1ea4f1e14a29c1f935b4de5eba8478435
bowen0701 <bowen0701@gmail.com>
2019-10-07 22:46:02 +0200

    Add random_sample.py

---
+++ random_sample.py
@@ -0,0 +1,21 @@
+"""Sample random variable."""
+
+from __future__ import absolute_import, division, print_function
+
+import random
+
+
+class SampleDiscreteRV(object):
+    def __init__(self, values, probs=None, n_bins=1000):
+        pass
+
+    def sample(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
