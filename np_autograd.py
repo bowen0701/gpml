e0de2a05609229ae632ae63cc8f21f7427ffbda1
bowen0701 <bowen0701@gmail.com>
2020-03-29 18:32:54 +0200

    Implementing autograd

--- np_autograd.py
+++ np_autograd.py
@@ -6,15 +6,39 @@ import numpy as np
 
 
 class Tensor(object):
-	"""Automatic differentiation."""
+    """Automatic differentiation."""
 
-	def __init__(self):
-		pass
+    def __init__(self, x, requires_grad=False, dependency=None):
+        self._x = x
+        self._shape = self._x.shape
+        self._grad = None
+
+        if requires_grad:
+            self.zero_grad()
+        self._require_grad = requires_grad
+
+        if not dependency:
+            dependency = []
+        self._dependency = dependency
+
+    @property
+    def values(self):
+        return self._x
+
+    @values.setter
+    def values(self, x_new):
+        self._x = np.array(x_new)
+        self._grad = None
+
+    def zero_grad(self):
+        self.grad = np.zeros(self._shape)
+
+    # TODO: Continue implementing autograd.
 
 
 def main():
-	pass
+    pass
 
 
 if __name__ == '__main__':
-	main()
+    main()
