f8d317cde810feed113338a8050eaa97dfbe0b4a
bowen0701 <bowen0701@gmail.com>
2020-03-29 15:16:11 +0200

    Add np_autograd.py

---
+++ np_autograd.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+
+
+class Tensor(object):
+	"""Automatic differentiation."""
+
+	def __init__(self):
+		pass
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
