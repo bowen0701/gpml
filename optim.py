6a4e932e8c48c01b0e06d9f095b9715fb09cb36e
bowen0701 <bowen0701@gmail.com>
2020-08-11 20:10:46 +0800

    Add optim.py

---
+++ optim.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
