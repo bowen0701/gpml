f1ac87b6b945ebad5e548521c89b54681d5fc121
bowen0701 <bowen0701@gmail.com>
2020-09-24 22:59:58 +0100

    Impl linreg in PyTorch

--- linear_regression.py
+++ linear_regression.py
@@ -98,7 +98,96 @@ class LinearRegression(object):
         return self._model(X).reshape((-1,))
 
 
-# TODO: Implement linear regression in PyTorch.
+# TODO: Continue implementing linear regression in PyTorch.
+class LinearRegressionTorch(object):
+    """Numpy implementation of Linear Regression."""
+
+    def __init__(self, batch_size=64, lr=0.01, n_epochs=1000):
+        self.batch_size = batch_size
+        self.lr = lr
+        self.n_epochs = n_epochs
+
+    def get_data(self, X_train, y_train, shuffle=True):
+        """Get dataset and information."""
+        self.X_train = X_train
+        self.y_train = y_train
+
+        # Get the numbers of examples and inputs.
+        self.n_examples, self.n_inputs = self.X_train.shape
+
+        if shuffle:
+            idx = list(range(self.n_examples))
+            random.shuffle(idx)
+            self.X_train = self.X_train[idx]
+            self.y_train = self.y_train[idx]
+
+    def _create_weights(self):
+        """Create model weights and bias."""
+        # self.w = np.zeros(self.n_inputs).reshape(self.n_inputs, 1)
+        # self.b = np.zeros(1).reshape(1, 1)
+        pass
+
+    def _model(self, X):
+        """Linear regression model."""
+        # return np.matmul(X, self.w) + self.b
+        pass
+
+    def _loss(self, y, y_):
+        """Squared error loss.
+
+        # squared_error_loss(y, y_) 
+        #   = - 1/n * \sum_{i=1}^n (y_i - y__i)^2
+        """
+        # self.squared_error = np.square(y - y_)
+        # return np.mean(self.squared_error)
+        pass
+
+    def _optimize(self, X, y):
+        """Optimize by stochastic gradient descent."""
+        # m = X.shape[0]
+
+        # y_ = self._model(X) 
+        # dw = 1 / m * np.matmul(X.T, y_ - y)
+        # db = np.mean(y_ - y)
+
+        # for (param, grad) in zip([self.w, self.b], [dw, db]):
+        #     param[:] = param - self.lr * grad
+        pass
+
+    def _fetch_batch(self):
+        """Fetch batch dataset."""
+        idx = list(range(self.n_examples))
+        for i in range(0, self.n_examples, self.batch_size):
+            idx_batch = idx[i:min(i + self.batch_size, self.n_examples)]
+            yield (self.X_train.take(idx_batch, axis=0), 
+                   self.y_train.take(idx_batch, axis=0))
+
+    def fit(self):
+        """Fit model."""
+        # self._create_weights()
+
+        # for epoch in range(self.n_epochs):
+        #     total_loss = 0
+        #     for X_train_b, y_train_b in self._fetch_batch():
+        #         y_train_b = y_train_b.reshape((y_train_b.shape[0], -1))
+        #         self._optimize(X_train_b, y_train_b)
+        #         train_loss = self._loss(y_train_b, self._model(X_train_b))
+        #         total_loss += train_loss * X_train_b.shape[0]
+
+        #     if epoch % 100 == 0:
+        #         print('epoch {0}: training loss {1}'
+        #               .format(epoch, total_loss / self.n_examples))
+        pass
+
+        return self
+
+    def get_coeff(self):
+        # return self.b, self.w.reshape((-1,))
+        pass
+
+    def predict(self, X):
+        # return self._model(X).reshape((-1,))
+        pass
 
 
 def reset_tf_graph(seed=71):
