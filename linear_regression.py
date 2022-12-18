3b78912cef652f2fcccd16097da96c35dfa147df
bowen0701 <bowen0701@gmail.com>
2022-12-18 08:35:55 +0000

    Remove random seed first

--- linear_regression.py
+++ linear_regression.py
@@ -2,10 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import random
 import numpy as np
-
-np.random.seed(71)
 
 
 class LinearRegression:
