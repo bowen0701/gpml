01191d07d3f8105384cea51d9a4ce0697c8805c5
bowen0701 <bowen0701@gmail.com>
2022-12-18 08:37:40 +0000

    Remove imports: random & optim

--- logistic_regression_torch.py
+++ logistic_regression_torch.py
@@ -2,13 +2,11 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import random
 import numpy as np
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 
 
 class LogisticRegression(nn.Module):
