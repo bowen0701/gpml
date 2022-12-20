dde536cd63c0cc0d182e892e98abf92981a36b8a
bowen0701 <bowen0701@gmail.com>
2022-12-20 16:15:43 +0000

    Complete impl for softmax_regression_torch.py

--- softmax_regression_torch.py
+++ softmax_regression_torch.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 from __future__ import absolute_import
 from __future__ import division
@@ -7,8 +8,20 @@ import numpy as np
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from torch import Tensor
 
 
 class SoftmaxRegression(nn.Module):
-    """TODO: PyTorch implementation of Softmax Regression."""
+    """PyTorch implementation of Softmax Regression."""
+    def __init__(self, input_dim: int, output_dim: int) -> None:
+        super().__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        # Softmaz regression.
+        self.fc1 = nn.Linear(self.input_dim, self.output_dim)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.fc1(x)
+        x = self.softmax(x)
+        return x
