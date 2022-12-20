e02655842e23ee173a19811be6d3e3704be999fe
bowen0701 <bowen0701@gmail.com>
2022-12-20 10:34:07 +0000

    Complete impl for logistic_regression_torch.py

--- logistic_regression_torch.py
+++ logistic_regression_torch.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 from __future__ import absolute_import
 from __future__ import division
@@ -7,8 +8,20 @@ import numpy as np
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from torch import Tensor
 
 
 class LogisticRegression(nn.Module):
-    """TODO: PyTorch implementation of Linear Regression."""
+    """PyTorch implementation of Linear Regression."""
+
+    def __init__(self, input_dim: int) -> None:
+        super().__init__()
+        self.input_dim = input_dim
+        # Logistic regression.
+        self.fc1 = nn.Linear(input_dim)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.fc1(self.fc1)
+        x = self.sigmoid(x)
+        return x
