ef47098286384a00852114b8cdc153fdd30267d7
bowen0701 <bowen0701@gmail.com>
2022-12-25 20:58:02 +0000

    Unify future imports

--- autograd.py
+++ autograd.py
@@ -1,6 +1,6 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import, division, print_function
 
 import numpy as np
 
