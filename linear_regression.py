c602e47c93dc20dc18d6de3831ab7e8558970b2c
bowen0701 <bowen0701@gmail.com>
2022-12-29 17:26:31 +0000

    Rename to np files & torch files w/o suffix

--- linear_regression.py
+++ linear_regression.py
@@ -4,88 +4,20 @@ from __future__ import absolute_import, division, print_function
 
 import numpy as np
 
+import torch
+import torch.nn as nn
+from torch import Tensor
 
-class LinearRegression:
-    """Numpy implementation of Linear Regression."""
 
-    def __init__(self, batch_size=64, lr=0.01, n_epochs=1000):
-        self.batch_size = batch_size
-        self.lr = lr
-        self.n_epochs = n_epochs
+class LinearRegression(nn.Module):
+    """PyTorch implementation of Linear Regression."""
 
-    def get_data(self, X_train, y_train, shuffle=True):
-        """Get dataset and information."""
-        self.X_train = X_train
-        self.y_train = y_train
+    def __init__(self, input_dim: int) -> None:
+        super().__init__()
+        self.input_dim = input_dim
+        # Linear regression.
+        self.fc1 = nn.Linear(self.input_dim, 1)
 
-        # Get the numbers of examples and inputs.
-        self.n_examples, self.n_inputs = self.X_train.shape
-
-        if shuffle:
-            idx = list(range(self.n_examples))
-            random.shuffle(idx)
-            self.X_train = self.X_train[idx]
-            self.y_train = self.y_train[idx]
-
-    def _create_weights(self):
-        """Create model weights and bias."""
-        self.w = np.zeros(self.n_inputs).reshape(self.n_inputs, 1)
-        self.b = np.zeros(1).reshape(1, 1)
-
-    def _model(self, X):
-        """Linear regression model."""
-        return np.matmul(X, self.w) + self.b
-
-    def _loss(self, y, y_):
-        """Squared error loss.
-
-        # squared_error_loss(y, y_) 
-        #   = - 1/n * \sum_{i=1}^n (y_i - y__i)^2
-        """
-        self.squared_error = np.square(y - y_)
-        return np.mean(self.squared_error)
-
-    def _optimize(self, X, y):
-        """Optimize by stochastic gradient descent."""
-        m = X.shape[0]
-
-        y_ = self._model(X) 
-        dw = 1 / m * np.matmul(X.T, y_ - y)
-        db = np.mean(y_ - y)
-
-        for (param, grad) in zip([self.w, self.b], [dw, db]):
-            param[:] = param - self.lr * grad
-
-    def _fetch_batch(self):
-        """Fetch batch dataset."""
-        idx = list(range(self.n_examples))
-        for i in range(0, self.n_examples, self.batch_size):
-            idx_batch = idx[i:min(i + self.batch_size, self.n_examples)]
-            yield (self.X_train.take(idx_batch, axis=0), 
-                   self.y_train.take(idx_batch, axis=0))
-
-    def fit(self):
-        """Fit model."""
-        self._create_weights()
-
-        for epoch in range(self.n_epochs):
-            total_loss = 0
-            for X_train_b, y_train_b in self._fetch_batch():
-                y_train_b = y_train_b.reshape((y_train_b.shape[0], -1))
-                self._optimize(X_train_b, y_train_b)
-                batch_loss = self._loss(y_train_b, self._model(X_train_b))
-                total_loss += batch_loss * X_train_b.shape[0]
-
-            if epoch % 100 == 0:
-                print('epoch {0}: training loss {1}'
-                      .format(epoch, total_loss / self.n_examples))
-
-        return self
-
-    def get_coeff(self):
-        """Get model coefficients."""
-        return self.b, self.w.reshape((-1,))
-
-    def predict(self, X):
-        """Predict for new data."""
-        return self._model(X).reshape((-1,))
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.fc1(x)
+        return x
