802550a31369552c2cf9241057da65e1af0e1209
bowen0701 <bowen0701@gmail.com>
2023-05-11 22:58:56 +0100

    Move to todo

--- optim.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-from __future__ import absolute_import, division, print_function
-
-import numpy as np
-
-# TODO: Implement optimization methods for machine learninig.
