c991603c33821ffc8664f8391c1559acb60e4dc8
bowen0701 <bowen0701@gmail.com>
2023-12-31 16:09:10 +0000

    Complete GutenburgLoader's load_top_books()

--- load_gutenburg.py
+++ load_gutenburg.py
@@ -2,3 +2,56 @@
 # -*- coding:utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
+class GutenburgLoader:
+    def __init__(self, top_books_category: str = "Top 100 EBooks yesterday"):
+        self.frequently_download_url = "https://www.gutenberg.org/browse/scores/top"
+        self.top_books_category = top_books_category
+
+    @staticmethod
+    def get_request(url: str) -> Response:
+        req = requests.get(url)
+        try:
+            req.raise_for_status()
+        except Exception as exc:
+            print(f"There was a problem: {exc}")
+        return req
+    
+    def get_top_book_names_urls(self) -> Dict[str, str]:
+        gutenberg_req = GutenburgLoader.get_request(self.frequently_download_url)
+        gutenberg_soup = bs4.BeautifulSoup(gutenberg_req.text)
+
+        self.top_book_names_urls = dict()
+
+        for title, element in zip(gutenberg_soup.find_all("h2"), gutenberg_soup.find_all('ol')):
+            if title.text == self.top_books_category:
+                print(f"Getting {title.text}'s names and URLs")
+                for line in element.find_all("li"):
+                    book_name = line.get_text()
+                    book_id = line.a["href"].split("/")[-1]
+                    book_url = f"https://www.gutenberg.org/cache/epub/{book_id}/pg{book_id}.txt"
+                    self.top_book_names_urls[book_name] = book_url
+        
+        return self.top_book_names_urls
+    
+
+    def load_top_books(self):
+        for book_name, book_url in iter(self.top_book_names_urls.items()):
+            book_title = book_name.rsplit(" by", maxsplit=1)[0]
+            print(f"book_title: {book_title}")
+            
+            req = GutenburgLoader.get_request(book_url)
+            book_text = req.text
+            print("Starting truncating book text.")
+
+            # Truncate head substring.
+            truncate_head_substr = f"*** START OF THE PROJECT GUTENBERG EBOOK {book_title.upper()} ***"
+            book_text = book_text.split(truncate_head_substr)[1]
+            print(f"Truncate head substring is done!")
+
+            # Truncate tail substring.
+            truncate_tail_substr = f"*** END OF THE PROJECT GUTENBERG EBOOK {book_title.upper()} ***"
+            book_text = book_text.split(truncate_tail_substr)[0]
+            print(f"Truncate tail substring is done!")
+            
+            # TODO: Preprocessing text.
+            break
